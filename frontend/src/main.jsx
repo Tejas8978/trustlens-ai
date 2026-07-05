adc64d802f3e69c7a73b7f1fcbfe22c1b8370a9b
Tejas8978 <tejasaravapalli@gmail.com>
2026-07-05 20:15:53 +0530

    fix: force Vercel redeployment with correct CSS properties

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Root mounting point for the Vite + React app
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
